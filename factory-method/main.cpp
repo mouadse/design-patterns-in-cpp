9c2a74246863aa56e00bfcf36ec1cff6df382db0
Mouad <mouadse@gmail.com>
2025-07-06 17:01:22 +0100

    X

--- factory-method/main.cpp
+++ factory-method/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include <string>
 
+class Product {
+public:
+  virtual ~Product() {}
+  virtual std::string Operation() const = 0;
+};
+
+class ConcreteProduct1 : public Product {
+public:
+  std::string Operation() const override {
+    return "{Result of ConcreteProduct1}";
+  }
+};
+
+class ConcreteProduct2 : public Product {
+public:
+  std::string Operation() const override {
+    return "{Result of ConcreteProduct2}";
+  }
+};
+
+class Creator {
+public:
+  virtual ~Creator() {}
+  virtual Product *FactoryMethod() const = 0;
+  std::string SomeOperation() {
+    Product *prd = this->FactoryMethod();
+    std::string result =
+        "Creator: The same creator's code has just worked with " +
+        prd->Operation();
+    delete prd;
+    return result;
+  }
+};
+
 int main() {
   // Code demo goes here
   return 0;
